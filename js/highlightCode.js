4f0c6950c5123a0b612e5d3f97fc983af20d525d
Brian Capouch <brianc@saintjoe.edu>
2015-04-08 03:29:16 -0400

    Note where this code went, and how it is referenced

---
+++ js/highlightCode.js
@@ -0,0 +1,17 @@
+function highlightCode(node, keywords) {
+  var text = node.textContent;
+  node.textContent = ""; // Clear the node
+
+  var match, pos = 0;
+  while (match = keywords.exec(text)) {
+    var before = text.slice(pos, match.index);
+    node.appendChild(document.createTextNode(before));
+    var strong = document.createElement("strong");
+    strong.appendChild(document.createTextNode(match[0]));
+    node.appendChild(strong);
+    pos = keywords.lastIndex;
+  }
+  var after = text.slice(pos);
+  node.appendChild(document.createTextNode(after));
+}
+
